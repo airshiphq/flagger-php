238a20da8e2efe0729449183346bf5eb69897ac8
George Li <glusa8@berkeley.edu>
2018-10-06 19:43:50 -0700

    php version

--- src/Flagger/Client/ClientInterface.php
+++ src/Flagger/Client/ClientInterface.php
@@ -5,7 +5,7 @@ namespace Flagger\Client;
 interface ClientInterface
 {
     const PLATFORM = 'php';
-    const VERSION = '0.3.0';
+    const VERSION = '1.0.0';
 
     const DEFAULT_SERVER_URL = 'http://localhost:5000';
     const OBJECT_GATE_VALUES_ENDPOINT = '/v2/object-values/';
